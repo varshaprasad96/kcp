68d740d8fd4eb609d6e9fc9b51d89703e80d3dea
David Festal <dfestal@redhat.com>
2021-09-02 13:30:56 +0200

    Add describe nodes
    Signed-off-by: David Festal <dfestal@redhat.com>

--- contrib/demo/kubecon-removeClusters.sh
+++ contrib/demo/kubecon-removeClusters.sh
@@ -3,8 +3,10 @@
 DEMO_ROOT="$(dirname "${BASH_SOURCE}")"
 CLUSTERS_DIR=${DEMO_ROOT}/clusters/kind
 
+kubectl --kubeconfig=${CLUSTERS_DIR}/us-west1.kubeconfig describe nodes >>us-west1.log 2>&1
 kubectl --kubeconfig=${CLUSTERS_DIR}/us-west1.kubeconfig get deployment my-deployment--us-west1 -o yaml -n demo >>us-west1.log 2>&1
 kubectl --kubeconfig=${CLUSTERS_DIR}/us-west1.kubeconfig describe pods -n demo >>us-west1.log 2>&1
+kubectl --kubeconfig=${CLUSTERS_DIR}/us-west1.kubeconfig describe nodes >>us-east1.log 2>&1
 kubectl --kubeconfig=${CLUSTERS_DIR}/us-east1.kubeconfig get deployment my-deployment--us-east1 -o yaml -n demo >>us-east1.log 2>&1
 kubectl --kubeconfig=${CLUSTERS_DIR}/us-east1.kubeconfig describe pods -n demo >>us-east1.log 2>&1
 
