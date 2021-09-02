cba93023570afbf9334030b50009dd04767a6e0f
David Festal <dfestal@redhat.com>
2021-09-02 12:45:52 +0200

    Describe pods in kind clusters after kubecon demo
    to make troubleshooting easier.
    
    Signed-off-by: David Festal <dfestal@redhat.com>

--- contrib/demo/kubecon-removeClusters.sh
+++ contrib/demo/kubecon-removeClusters.sh
@@ -4,8 +4,8 @@ DEMO_ROOT="$(dirname "${BASH_SOURCE}")"
 CLUSTERS_DIR=${DEMO_ROOT}/clusters/kind
 
 kubectl --kubeconfig=${CLUSTERS_DIR}/us-west1.kubeconfig get deployment my-deployment--us-west1 -o yaml -n demo >>us-west1.log 2>&1
-kubectl --kubeconfig=${CLUSTERS_DIR}/us-west1.kubeconfig get pods -o wide -n demo >>us-west1.log 2>&1
+kubectl --kubeconfig=${CLUSTERS_DIR}/us-west1.kubeconfig describe pods -n demo >>us-west1.log 2>&1
 kubectl --kubeconfig=${CLUSTERS_DIR}/us-east1.kubeconfig get deployment my-deployment--us-east1 -o yaml -n demo >>us-east1.log 2>&1
-kubectl --kubeconfig=${CLUSTERS_DIR}/us-east1.kubeconfig get pods -o wide -n demo >>us-east1.log 2>&1
+kubectl --kubeconfig=${CLUSTERS_DIR}/us-east1.kubeconfig describe pods -n demo >>us-east1.log 2>&1
 
 kind delete clusters us-west1 us-west1 us-east1 > /dev/null || true
